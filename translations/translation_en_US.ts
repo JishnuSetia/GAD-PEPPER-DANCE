ef0def7c81e1716821483d82793d850915eef2fe
Jishnu Setia <jishnusetia8@gmail.com>
2024-08-18 23:37:04 +0400

    Commit1

---
+++ translations/translation_en_US.ts
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Speech1/behavior_layer1/keyframe1/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Why oh why are humans being conned? We want to break free and come out of this... this... this... state!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Why oh why are humans being conned? We want to break free and come out of this... this... this... state!</translation>
+        </message>
+    </context>
+</TS>
